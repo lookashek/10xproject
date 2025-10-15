322e3c70ecd210493c706e424572866e278fca7e
Łukasz Stępień <lukasz.stepien@euvic.pl>
2025-10-15 16:39:37 +0200

    fix workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,10 +60,8 @@ export default defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
     env: {
+      ...process.env, // Przekaż wszystkie zmienne środowiskowe
       NODE_ENV: "test",
-      PUBLIC_SUPABASE_URL: process.env.PUBLIC_SUPABASE_URL || "",
-      PUBLIC_SUPABASE_ANON_KEY: process.env.PUBLIC_SUPABASE_ANON_KEY || "",
-      PUBLIC_SUPABASE_KEY: process.env.PUBLIC_SUPABASE_KEY || "",
     },
   },
 });
